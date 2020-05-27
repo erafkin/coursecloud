3e84bf230824a32807ce53b6218d68d46096a819
Emma Rafkin <emmarafkin@gmail.com>
2020-05-27 16:57:33 -0400

    ranker setup, ranking not happening yet

--- screens/RankerScreen.js
+++ screens/RankerScreen.js
@@ -1,11 +1,96 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, TouchableHighlight } from 'react-native-gesture-handler';
+import { Dropdown } from 'react-native-material-dropdown';
+import axios from 'axios';
+import styles from '../styles/style';
 
-export default function RankerScreen() {
-  return (
-    <ScrollView>
-      <Text>Ranker</Text>
-    </ScrollView>
-  );
+class RankerScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject1: '',
+      course1: '',
+      subject2: '',
+      course2: '',
+
+    };
+  }
+
+  changeSubject = (val, num) => {
+    if (num === 1) {
+      this.setState({
+        subject1: val,
+      });
+    } else if (num === 2) {
+      this.setState({
+        subject2: val,
+      });
+    }
+  }
+
+  changeCourse = (val, num) => {
+    if (num === 1) {
+      this.setState({
+        course1: val,
+      });
+    } else if (num === 2) {
+      this.setState({
+        course2: val,
+      });
+    }
+  }
+
+  rank = () => {
+    const {
+      course1, course2, subject1, subject2,
+    } = this.state;
+    console.log(course1);
+    console.log(course2);
+    console.log(subject1);
+    console.log(subject2);
+  }
+
+
+  render() {
+    const { subject1, subject2 } = this.state;
+    const subjects = [{ value: 'COSC' }, { value: 'ENGS' }, { value: 'ECON' }, { value: 'GOV' }, { value: 'PSYC' }];
+    const courses = {
+      COSC: [{ value: '1' }, { value: '10' }, { value: '11' }, { value: '16' }, { value: '22' }, { value: '24' }],
+      ECON: [{ value: '1' }, { value: '2' }, { value: '5' }, { value: '10' }, { value: '20' }, { value: '21' }, { value: '22' }, { value: '24' }, { value: '25' }],
+      ENGS: [{ value: '2' }, { value: '3' }, { value: '8' }, { value: '11' }, { value: '12' }, { value: '20' }, { value: '21' }, { value: '22' }, { value: '24' }, { value: '25' }],
+      GOV: [{ value: '3' }, { value: '4' }, { value: '5' }, { value: '10' }, { value: '16' }, { value: '18' }],
+      PSYC: [{ value: '1' }, { value: '6' }, { value: '10' }, { value: '11' }, { value: '28' }, { value: '35' }, { value: '36' }, { value: '37' }, { value: '38' }],
+    };
+
+    return (
+      <ScrollView>
+        <Dropdown
+          label="Subject"
+          data={subjects}
+          onChangeText={(val) => this.changeSubject(val, 1)}
+        />
+        <Dropdown
+          label="Course"
+          data={subject1 === '' ? [] : courses[subject1]}
+          onChangeText={(val) => this.changeCourse(val, 1)}
+        />
+        <Text>VS</Text>
+        <Dropdown
+          label="Subject"
+          data={subjects}
+          onChangeText={(val) => this.changeSubject(val, 2)}
+        />
+        <Dropdown
+          label="Course"
+          data={subject2 === '' ? [] : courses[subject2]}
+          onChangeText={(val) => this.changeCourse(val, 2)}
+        />
+        <TouchableHighlight style={styles.subjectButton} onPress={() => { this.rank(); }}><Text style={styles.buttonWords}>Go!</Text></TouchableHighlight>
+
+
+      </ScrollView>
+    );
+  }
 }
+export default RankerScreen;
