b2f549697de11477f422db83e88fda44a20a4ab9
Emma Rafkin <emmarafkin@gmail.com>
2020-05-27 23:29:03 -0400

    started chatbot, not much done

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,11 +1,20 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
-export default function ChatbotScreen() {
-  return (
-    <ScrollView>
-      <Text>Chatbot</Text>
-    </ScrollView>
-  );
-}
+class ChatbotScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Text>Chatbot</Text>
+      </ScrollView>
+    );
+  }
+} export default ChatbotScreen;
