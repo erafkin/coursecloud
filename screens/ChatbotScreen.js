674adaf8fb61db861860074ca97f2edf53390619
Emma Rafkin <emmarafkin@gmail.com>
2020-05-28 17:52:45 -0400

    chatbot is working

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,20 +1,124 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import {
+  Text, TouchableHighlight, TextInput, View,
+} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import axois from 'axios';
+import styles from '../styles/style';
 
 class ChatbotScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      sessionId: '',
+      messages: [],
+      currMessage: '',
     };
   }
 
-  render() {
-    return (
-      <ScrollView>
-        <Text>Chatbot</Text>
-      </ScrollView>
+  startNewSession = () => {
+    axois.get('http://localhost:3030/api/chatbot/session').then((response) => {
+      this.setState({ sessionId: response.data });
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  handleMessage = (m, user) => {
+    const { messages } = this.state;
+    messages.push({ message: m, fromWatson: (user !== 'client'), key: messages.length });
+    this.setState({
+      messages,
+    });
+  }
+
+  messageAPI = (message) => {
+    const testSessionID = 'c010ea64-570d-416c-898d-93c69421cd59';
+    axois.post('http://localhost:3030/api/chatbot/message', { sessionId: testSessionID, text: message })
+      .then((response) => {
+        this.handleMessage(response.data.response.output.generic[0].text, 'watson');
+      });
+  }
+
+  changeMessage = (text) => {
+    this.setState({
+      currMessage: text,
+    });
+  }
+
+  messageScreen = () => {
+    const { messages } = this.state;
+    if (messages.length === 0) {
+      return <Text>no messages</Text>;
+    } else {
+      return (
+        <ScrollView style={{ maxHeight: 430, backgroundColor: 'blue' }}>
+          {this.indivMessage(messages)}
+        </ScrollView>
+      );
+    }
+  }
+
+  indivMessage = (messages) => {
+    return (messages.map((mes) => {
+      console.log(mes);
+      return mes.fromWatson
+        ? (
+          <View style={styles.watsonMessageContainer} key={mes.key}>
+            <View style={styles.watsonMessage}>
+              <Text style={styles.watsonMessageWords}>
+                {mes.message}
+              </Text>
+            </View>
+          </View>
+        )
+        : (
+          <View style={styles.clientMessageContainer} key={mes.key}>
+            <View style={styles.clientMessage}>
+              <Text style={styles.clientMessageWords}>
+                {mes.message}
+              </Text>
+            </View>
+          </View>
+        );
+    })
     );
+  }
+
+
+  render() {
+    const {
+      currMessage, sessionId,
+    } = this.state;
+    if (sessionId === '') {
+      return (
+        <ScrollView>
+          <TouchableHighlight onPress={() => { this.startNewSession(); }} style={styles.newSession}>
+            <Text style={styles.newSessionWords}> Start a conversation!</Text>
+          </TouchableHighlight>
+        </ScrollView>
+      );
+    } else {
+      return (
+        <View style={{ flexDirection: 'column', height: '100%' }}>
+          {this.messageScreen()}
+          <View style={styles.chatbotInput}>
+            <TextInput
+              style={{
+                height: 40, borderColor: 'gray', borderWidth: 1, borderRadius: 10, paddingLeft: 10,
+              }}
+              onChangeText={(text) => this.changeMessage(text)}
+              placeholder="type here"
+              value={currMessage}
+            />
+            <TouchableHighlight onPress={() => { this.handleMessage(currMessage, 'client'); this.messageAPI(currMessage); }} style={styles.courseButton}>
+              <Text style={styles.buttonWords}> send</Text>
+            </TouchableHighlight>
+          </View>
+
+
+        </View>
+      );
+    }
   }
 } export default ChatbotScreen;
--- styles/style.js
+++ styles/style.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default StyleSheet.create({
   subjectButton: {
@@ -31,5 +32,59 @@ export default StyleSheet.create({
     alignItems: 'center',
     width: '100%',
     borderRadius: 5,
+  },
+  watsonMessageContainer: {
+    alignItems: 'flex-start',
+    width: '100%',
+
+  },
+  clientMessageContainer: {
+    alignItems: 'flex-end',
+    width: '100%',
+
+
+  },
+  watsonMessage: {
+    backgroundColor: '#00693e',
+    maxWidth: '45%',
+    borderRadius: 20,
+    margin: '5%',
+  },
+  clientMessage: {
+    backgroundColor: 'lightgray',
+    maxWidth: '45%',
+    borderRadius: 20,
+    margin: '5%',
+
+  },
+  watsonMessageWords: {
+    margin: 10,
+    color: 'white',
+  },
+  clientMessageWords: {
+    margin: 10,
+    color: 'black',
+  },
+  chatbotInput: {
+    position: 'absolute',
+    bottom: 10,
+    width: '90%',
+    alignSelf: 'center',
+  },
+  newSession: {
+    width: '50%',
+    backgroundColor: '#00693e',
+    padding: '2%',
+    alignItems: 'center',
+    borderRadius: 5,
+    margin: '1%',
+    alignSelf: 'center',
+  },
+  newSessionWords: {
+    color: '#FFFFFF',
+    fontSize: 14,
+    margin: '3%',
+    width: 200,
+    textAlign: 'center',
   },
 });
